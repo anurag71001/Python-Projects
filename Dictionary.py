58524d8c8c01f81c675b01d8a7ad553b5fef9231
Anurag Khanna <71001anu@gmail.com>
2021-12-11 15:16:21 +0530

    Dictionary file added

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+D = {"Abandon": " to stop doing something before it is finished, or to stop following a plan, idea,etc.",
+     "Able": "good at doing something",
+     "Abnormal":"different from what is normal or usual, in a way which is strange or dangerous",
+      "Absolutely" : "used to strongly agree with someone"}
+Name = input("Enter the word: ")
+print(D[Name])
