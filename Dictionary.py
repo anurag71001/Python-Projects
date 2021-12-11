65c3266822aa1552e64e57283565da9438f4ddf6
Anurag Khanna <71001anu@gmail.com>
2021-12-11 15:25:45 +0530

    made some changes in Dictionary file

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,5 @@
-D = {"Abandon": " to stop doing something before it is finished, or to stop following a plan, idea,etc.",
+D = {
+     "Abandon": "to stop doing something before it is finished, or to stop following a plan, idea,etc.",
      "Able": "good at doing something",
      "Abnormal":"different from what is normal or usual, in a way which is strange or dangerous",
       "Absolutely" : "used to strongly agree with someone"}
